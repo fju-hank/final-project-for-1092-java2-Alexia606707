f31f9dbf2ed0014c027ff5e3a9ade07adb4b52fe
Alexia606707 <rebeccaho717@gmail.com>
2021-06-28 13:13:08 +0800

    MenuDemo form

---
+++ src/main/java/com/fju/MenuDemo.java
@@ -0,0 +1,14 @@
+package com.fju;
+
+import javax.swing.*;
+
+public class MenuDemo {
+    private JPanel main;
+    private JPanel Menu;
+    private JTextPane menu;
+    private JPanel Meals;
+    private JTextField Hamburger;
+    private JTextField FrenchFries;
+    private JTextField Drinks;
+    private JTextField IceCream;
+}
